260acf2121457b578e7f395e712fd64803c057a0
PRATHAMTEJANI <prathamtejani4@gmail.com>
2025-07-27 10:48:30 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,96 @@
+console.log("pratham tejani")
+// console.log("pratham tejani")
+// console.log("pratham tejani")
+// console.log("pratham tejani")
+// console.log("pratham tejani")
+
+
+// variables in js
+// dynamic language
+// var a = 30
+// console.log(a)
+// let b= 100
+// console.log(b)
+// const c = "variable declaration types"
+// console.log(c)
+// a = null
+// b = undefined
+// console.log(a,b)
+// a = true
+// console.log(a)
+
+// let x = 90
+// // let x = 20
+ 
+// console.log(x)
+
+// const p = 30
+// // p = 50
+// console.log(p)
+
+
+{
+    let a = 90
+  //  let a = 30
+    console.log(a)
+}
+{
+    let a = 40
+    console.log(a)
+}
+let m = BigInt(12344)
+console.log(m)
+
+const student =  //object declaration
+{
+    fullname : "pratham tejani",
+    age : 190,
+    cgpa : 12,
+    ispadd : true
+}
+student["age"] = 234  //acces of object variable and modification is doing in this line 
+console.log(student["age"])
+console.log(student)
+
+a = 3
+b = 3
+console.log(a + b + b + a)
+console.log(a / b)
+console.log(a * b)
+console.log(a - b)
+console.log(a % b)
+console.log(a++)
+console.log(++a)
+
+let x = -100;
+x <<= 5;
+console.log(x)
+console.log("a == b" , a == b)
+
+let age = 21
+if(age > 18 && age < 20)
+{
+    console.log("go for vote")
+}
+else if(age > 20 && age < 40)
+{
+    console.log(age , " go for vote with wife")
+}
+else{
+    console.log("not seat at hom")
+}
+
+//input
+let num = prompt("enter a number ")
+if(num % 2 == 0)
+{
+    console.log("even")
+}
+else
+{
+    console.log("odd")
+}
+
+
+
+
